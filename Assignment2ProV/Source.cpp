3b3ede119ab86289c5261234747e2ce4acecc69c
jiya <jpandit5253@conestogac.on.ca>
2024-09-18 13:54:14 -0400

    added the struct data and the other functionality which stores and parses student data

--- Assignment2ProV/Source.cpp
+++ Assignment2ProV/Source.cpp
@@ -1,8 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-int main() 
-
+struct STUDENT_DATA
 {
+    std::string FirstNameOfStudent;
+    std::string LastNameOfStudent;
+};
 
-    return 1;
+int main()
+{
+   
+    std::ifstream file("C:\\Users\\jiyap\\source\\repos\\Assignment2ProV\\StudentData.txt");
 
+    std::string line;
+    std::vector<STUDENT_DATA> Students;
+
+  
+    if (file.is_open())
+
+    {
+        
+        while (std::getline(file, line))
+        {
+            std::istringstream ss(line);
+
+            std::string FirstNameOfStudent, LastNameOfStudent;
+
+            
+            std::getline(ss, FirstNameOfStudent, ',');
+
+            std::getline(ss, LastNameOfStudent, ',');
+
+            
+            STUDENT_DATA Student{ FirstNameOfStudent, LastNameOfStudent };
+
+            Students.push_back(Student);
+        }
+
+        file.close();  
+    }
+
+    else
+
+    {
+        std::cerr << "Sorry, we are not able to open the file!!!";
+
+        return 1;  
+    }
+
+    
+    for (const auto& student : Students)
+
+    {
+        std::cout << "First Name: " << student.FirstNameOfStudent<< ", Last Name: " << student.LastNameOfStudent << std::endl;
+    }
+
+    return 0;
 }
